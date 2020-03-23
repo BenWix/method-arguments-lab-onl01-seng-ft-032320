30f8e59796c77c1a9dd836ab8a240e8ced9f9e53
Benjamin Wichser <benwichser@gmail.com>
2020-03-23 14:56:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
 
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program with #{language}.\n"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end 
